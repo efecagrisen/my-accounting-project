fd727a42d5bb785e120e91d70aff0af223595c4f
efecagrisen <efecagrisen@yahoo.com>
2024-07-31 15:11:12 +0300

    EUAC1-11 Invoice Entity & DTO and InvoiceType & InvoiceStatus enums created

---
+++ src/main/java/com/ecs/dto/InvoiceDto.java
@@ -0,0 +1,22 @@
+package com.ecs.dto;
+
+import com.ecs.enums.InvoiceStatus;
+import com.ecs.enums.InvoiceType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class InvoiceDto {
+
+    private Long id;
+    private String invoiceNo;
+    private InvoiceStatus invoiceStatus;
+    private InvoiceType invoiceType;
+    private LocalDate date;
+    private CompanyDto companyDto;
+    private ClientVendorDto clientVendorDto;
+    private BigDecimal price;
+    private BigDecimal tax;
+    private BigDecimal total;
+
+}
---
+++ src/main/java/com/ecs/enums/InvoiceType.java
@@ -0,0 +1,15 @@
+package com.ecs.enums;
+
+public enum InvoiceType {
+    PURCHASE("Purchase"),SALES("Sales");
+
+    private final String value;
+
+    InvoiceType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
