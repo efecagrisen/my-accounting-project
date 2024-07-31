7f56dbc2fdec90d8af9ac9499dff2aca227d2fcc
efecagrisen <efecagrisen@yahoo.com>
2024-07-31 09:15:21 +0300

    EUAC1-2 Create Project

---
+++ src/main/java/com/ecs/MyAccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAccountingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecs/MyAccountingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAccountingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
