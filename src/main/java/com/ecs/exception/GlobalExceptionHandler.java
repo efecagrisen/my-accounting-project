551e75c02146a215e6d8dc53a5384d35af1e0361
efecagrisen <efecagrisen@yahoo.com>
2024-09-27 15:24:28 +0300

    EUAC1-85 GlobalExceptionHandler class added

---
+++ src/main/java/com/ecs/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.ecs.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({
+            Throwable.class,
+            Exception.class,
+            RuntimeException.class
+    })
+    public ModelAndView handleError(HttpServletRequest req, Exception exception) throws Exception {
+
+        // Rethrow annotated exceptions or they will be processed here instead.
+        if (AnnotationUtils.findAnnotation(exception.getClass(),
+                ResponseStatus.class) != null)
+            throw exception;
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", exception);
+        mav.addObject("url", req.getRequestURL());
+        mav.addObject("timestamp", new Date().toString());
+        mav.addObject("status", 500);
+
+        mav.setViewName("error");
+        return mav;
+    }
+
+}
